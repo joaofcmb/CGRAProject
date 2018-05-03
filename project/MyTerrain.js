7daa8ebbb442219e9a4810992fbc81c89eb01946
gabicfa <gabicfa1996@gmail.com>
2018-05-03 16:15:10 +0100

    terreno

--- project/MyTerrain.js
+++ project/MyTerrain.js
@@ -11,8 +11,8 @@ class MyTerrain extends CGFobject{
     this.materialTerrain = new CGFappearance(this.scene);
     this.materialTerrain.setAmbient(0.8, 0.8, 0.8, 1);
     this.materialTerrain.setSpecular(0.3, 0.3, 0.3, 1);
-    this.materialTerrain.setDiffuse(0.8, 0.8, 0.8, 1);
-    this.materialTerrain.setShininess(20);
+    this.materialTerrain.setDiffuse(0.5, 0.5, 0.5, 1);
+    this.materialTerrain.setShininess(5);
     this.materialTerrain.loadTexture("../resources/images/RockyDesert.png");
   };
 
@@ -20,9 +20,9 @@ class MyTerrain extends CGFobject{
   {
     this.materialTerrain.apply();
     this.scene.pushMatrix();
-    this.scene.rotate(-Math.PI/2, 1, 0, 0);
-    this.scene.scale(50, 50, 1);
-    this.terrain.display();
+      this.scene.rotate(-Math.PI/2, 1, 0, 0);
+      this.scene.scale(50, 50, 1);
+      this.terrain.display();
     this.scene.popMatrix();
   };
 };
